d287f74727a347160e75fce301959c612cffce6c
Israel Nunes <israelnetonunes@gmail.com>
2025-07-13 14:58:10 -0300

    feat: add parallax effect on scroll behaviour

--- public/js/index.js
+++ public/js/index.js
@@ -15,3 +15,25 @@ if (scrollToBtn && subscriptionSection) {
     });
   });
 };
+
+// scroll animation
+const itensAnime = document.querySelectorAll('[data-anime]');
+
+const animeScroll = () => {
+  const windowTop = window.scrollY + window.innerHeight * 0.85;
+
+  itensAnime.forEach(element => {
+    if (windowTop > element.offsetTop) {
+      element.classList.add('animate');
+    } else {
+      element.classList.remove('animate');
+    }
+
+  })
+}
+
+animeScroll();
+
+window.addEventListener('scroll', () => {
+  animeScroll();
+})
